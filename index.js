9ab6133aeb3e2bd77b0d98230b0fbf7287f76772
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-26 22:55:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+destructivelyAppendKitten("Ralph")
+console.log(arr);
+
+
+destructivelyPrependKitten("hi");
+function destructivelyAppendKitten(name){
+kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+
+}
